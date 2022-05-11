4f51528a7023d6d707f9d25afafb8f23f913788c
Daniel Weber <2694496+funami-dev@users.noreply.github.com>
2022-05-11 18:59:51 +0200

    init commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,13 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <router-link to='/'>front</router-link>
+    <router-link to='/form'>ok</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
---
+++ pages/form.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <pre>Form</pre>
+  <Tutorial/>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'FormPage'
+}
+</script>
